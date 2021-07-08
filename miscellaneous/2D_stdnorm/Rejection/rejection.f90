145223dbf083857e01d5594e955543e35d651c76
swflynn <swflynn@uci.edu>
2021-07-08 13:47:38 -0700

    Update rejection code for gaussian distribution.

--- miscellaneous/2D_stdnorm/Rejection/rejection.f90
+++ miscellaneous/2D_stdnorm/Rejection/rejection.f90
@@ -2,12 +2,12 @@
 !                       Gaussian (Sobol + Rejection)
 !=============================================================================80
 !     Discussion:
-!Fortran 90 Implementation of the Rejection method for a pdf as described in
-!fortran numerical methods.
-!A 2D Gaussian Potential, with uniform enclosing function.
+!Fortran 90 Implementation of the Rejection method for a 2D-Gaussian pdf 
+!pseudo/quasi random points are generated over the domain [xmin,xmax] and then 
+!rejection is applied to generate a set of points according to the pdf.
 !==============================================================================!
 !     Modified:
-!17 September 2018
+!8 Just 2021
 !     Author:
 !Shane Flynn
 !==============================================================================!
@@ -16,24 +16,54 @@ implicit none
 !==============================================================================!
 !                             Global Variables
 !==============================================================================!
-!Dimen     ==>Dimensionality of the system
+!d                  ==>Coordinate dimensionality (x^i=x_1,x_2,...x_d)
 !==============================================================================!
-integer::Dimen
+integer::d
+double precision,parameter::pie=acos(-1d0)
 !==============================================================================!
 contains
 !==============================================================================!
-subroutine pdf_eval(x,p)
+subroutine pdf_eval(x,P)
 !==============================================================================!
-!2D Gaussian to transform uniform sobol distribution to Gaussian distribution
-!Uniform sobol dist. is transformed form [0,1] to [min,max](see sobol_stdnormal)
+!normalized 2D Gaussian 
 !==============================================================================!
-!x        ==>(Dimen) gridpoint Coordinates (uniform distibution)
-!p        ==>Gaussian Gridpoint
+!x        ==>(d) gridpoint Coordinates (uniform distibution)
+!P        ==>Gaussian Gridpoint
 !==============================================================================!
 implicit none
-double precision::x(Dimen),p
-p=exp(-(((x(1))**2)/2+((x(2))**2)/2))
+double precision::x(d),p
+P=(2.*pie)**(-d/2.)*exp(-0.5*sum(x(:)**2))
 end subroutine pdf_eval
+!==============================================================================!
+subroutine unif_point(flag,skip,x_unif,xmin,xmax)
+!==============================================================================!
+!Generate a Uniform Distribution of Pseudo-Random or Quasi-Random Points
+!Scale the points from [0,1] ==> [xmin,xmax]
+!Uses the Sobol Sequence from sobol.f90 made available by John Burkardt
+!https://people.sc.fsu.edu/~jburkardt/f_src/sobol/sobol.html under GNU LGPL
+!==============================================================================!
+!flag               ==>True==Pseudo-Random; False==Quasi-Random
+!skip               ==>Seed for the quasi-random number generator
+!x_unif             ==>(d) point from uniform distribtuion
+!xmin               ==>(d) Minimum of domain size
+!xmax               ==>(d) Maximum of domain size
+!==============================================================================!
+use sobol
+implicit none
+integer(kind=8)::skip
+double precision::x_unif(d),xmin(d),xmax(d)
+logical::flag
+!==============================================================================!
+if(flag)then
+    call random_number(x_unif)
+    x_unif=xmin+x_unif*(xmax-xmin)    !Scale uniform distribution to span domain
+else
+    x_unif=i8_sobol(int(d,8),skip)
+    x_unif=xmin+x_unif*(xmax-xmin)        
+endif
+end subroutine unif_point
+!==============================================================================!
+!==============================================================================!
 !==============================================================================!
 end module rej_mod
 !==============================================================================!
@@ -44,28 +74,34 @@ implicit none
 !==============================================================================!
 !                                Discussion
 !==============================================================================!
-!Nsobol        ==>Number of Grid Points
+!Npoints       ==>Number of Grid Points
 !count_acc     ==>Number of Accepted Points
 !count_rej     ==>Number of Rejections
 !pxy           ==>PDF Evaluation
 !accept        ==>Randomly Generated Acceptance Condition
-!z             ==>(Dimen) Quasi-Random Points
-!x             ==>(Dimen,Nsobol) Accepted Coordinates
+!z             ==>(d) Quasi-Random Points
+!x             ==>(d,Nsobol) Accepted Coordinates
 !==============================================================================!
-integer::Nsobol,count_acc,count_rej,i
+integer::Npoints,count_acc,count_rej,i
 integer*8::ii,skip                                        !sobol module needs *8
-double precision::pxy,accept
-double precision,allocatable::z(:),x(:,:)
+double precision::pxy,accept,ixmin,ixmax
+double precision,allocatable::z(:),x(:,:),xmin(:),xmax(:)
+logical::flag
 !==============================================================================!
 !                               Read Input File
 !==============================================================================!
-read(*,*) Dimen
-read(*,*) Nsobol
-skip=Nsobol
+read(*,*) d
+read(*,*) Npoints
+read(*,*) ixmin
+read(*,*) ixmax
+read(*,*) flag                     !true == pseudo-random, false == quasi-random
+skip=Npoints
 !==============================================================================!
 !                           Allocations/Initialize
 !==============================================================================!
-allocate(z(Dimen),x(Dimen,Nsobol))
+allocate(z(d),x(d,Npoints),xmin(d),xmax(d))
+xmin=ixmin          !in general you may want different cutoff for dif. dimension
+xmax=ixmax
 count_acc=0
 count_rej=0
 z=0d0
@@ -74,11 +110,11 @@ x=0d0
 !                       Store All Coordinates Generated
 !==============================================================================!
 open(unit=17,file='all_data.dat')
-do while(count_acc<Nsobol)
-    call sobol_stdnormal(Dimen,skip,z(:))
+do while(count_acc<Npoints)
+    call unif_point(flag,skip,z,xmin,xmax)
     write(17,*) z(:)
     call random_number(accept)
-    call pdf_eval(z(:),pxy)
+    call pdf_eval(z,pxy)
 !==============================================================================!
 !                           Accept/Reject Criteria
 !==============================================================================!
@@ -94,17 +130,14 @@ close(17)
 !                        Write Accepted Points to File
 !==============================================================================!
 open(unit=18,file='grid.dat')
-do i=1,Nsobol
+do i=1,Npoints
     write(18,*) x(:,i)
 enddo
 close(18)
 !==============================================================================!
 !                           Simulation Results
 !==============================================================================!
-open(unit=99,file='simulation.dat')
-write(99,*) 'Nsobol ', Nsobol
-write(99,*) 'Dimensionality', dimen
-write(99,*) 'Total Rejections', count_rej
-close(99)
-write(*,*) 'Hello Universe!'
+write(*,*) 'Npoints ==> ', Npoints
+write(*,*) 'Dimensionality', d
+write(*,*) 'Total Rejections', count_rej
 end program reject
